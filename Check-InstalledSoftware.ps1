ae50e240b3a317de379328061c5d05efc5977d97
Federico Lillacci <65031947+tsmagnum@users.noreply.github.com>
2022-05-10 14:12:04 +0200

    Update Check-InstalledSoftware.ps1

--- Check-InstalledSoftware.ps1
+++ Check-InstalledSoftware.ps1
@@ -1,36 +1,14 @@
-<#
-.SYNOPSIS
-This scripts checks if a specific software is installed on a Windows computer, producing a CSV report.
-
-.DESCRIPTION
-This scripts checks if a specific software is installed on a Windows computer, producing a CSV report.
-
-.NOTES
-To get the right software name for the $software variable, look for the registry key 
-"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" in a computer with the desired software installed: here there is 
-a subkey for every software installed; look for the "DisplayValue" item in the subkey of your software.
-You can add * as wildcards. 
-For example, if you want to check for the .NET FX 4.7.2 set $software to "Microsoft .NET Framework 4.7.2*" .
-
-Author: Federico Lillacci - Coesione Srl - https://github.com/tsmagnum
-
-Link - https://github.com/tsmagnum/Windows/blob/master/Check-InstalledSoftware.ps1
-#>
-
-#region user-variables - please modify the values before running this script!
+#$creds = Get-Credential
 #Set the logfile name and path before running the script
-$logFile = "C:\Scripts\SoftwareReport.csv"
-#Set the target computers in a text file, one per line.
-$targets = Get-Content "C:\Scripts\myComputers.txt"
-#Set the name of the target software, please see the NOTES section above
-$software = "Microsoft .NET Framework 4.7.2*"
-#endregion
-
-#Getting the user credential to run the check
-$creds = Get-Credential
+$logFile = "C:\temp\SoftwareReport.csv"
+#Set the target computers in a textfile 
+$targets = Get-Content "C:\Targets\ClientSede.txt"
+#Set the name of the target software
+$software = "*Password Solution*"
+#$software2 = "Microsoft .NET Framework 4.8*"
 
 #Creating the logfile
-Set-Content -Path $logFile -Value "Computer, InstallationStatus"
+Set-Content -Path $logFile -Value "Computer, InstallationStatus, WindowsBuild"
 
 foreach ($target in $targets)
 {
@@ -46,6 +24,9 @@ foreach ($target in $targets)
                 #Getting the installed software list
                 $softwareList = Invoke-Command -Credential $creds -ComputerName $target `
                     -ScriptBlock {Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*}
+
+                $windowsBuild = Invoke-Command -Credential $creds -ComputerName $target `
+                    -ScriptBlock {Get-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | Select-Object -ExpandProperty CurrentBuild}
             }
         catch 
             {
@@ -55,21 +36,21 @@ foreach ($target in $targets)
             }
         
         #Checking if the target software is present
-        $installedSw = $softwareList | Where-Object {$_.DisplayName -like $software}
+        $installedSw = @()
+        $installedSw += $softwareList | Where-Object {$_.DisplayName -like $software}
 
         If ( $installedSw.Count -gt 0)
             {
             Write-Host -ForegroundColor Green "The requested software is installed on $target"
-            Add-Content -Path $logFile -Value "$target,Installed" 
+            Add-Content -Path $logFile -Value "$target,Installed,$windowsBuild" 
             }
         else
            {
             Write-Host -ForegroundColor Red "The requested software is not installed on $target"
-            Add-Content -Path $logFile -Value "$target,Not Installed" 
+            Add-Content -Path $logFile -Value "$target,Not Installed,$windowsBuild" 
             }
     }
     
-    #The computer is offline, skipping the check
     else
         {
             Write-Host -ForegroundColor Red "$target is offline, skipping check"
@@ -78,8 +59,7 @@ foreach ($target in $targets)
     
     }
 
-    
-    #Displaying Report Summary
+    #Report Summary
     $data = Import-Csv -path $logFile
     $installedStatus = $data | where-object {$_.InstallationStatus -eq "Installed"}
     $notInstalledStatus = $data | where-object {$_.InstallationStatus -ne "Installed"}
